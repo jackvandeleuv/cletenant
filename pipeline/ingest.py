6f05b530b86bf1f867b557d43616a38a3e1ae37f
jackvandeleuv <106289788+jackvandeleuv@users.noreply.github.com>
2026-05-10 22:34:06 -0400

    add pipeline scripts

---
+++ pipeline/ingest.py
@@ -0,0 +1,97 @@
+import requests
+import json 
+
+def get_params(offset, outfields='*'):
+    if type(outfields) == str:
+        outfields = ','.join(outfields)
+    return {
+        'resultRecordCount': 1000,
+        'resultOffset': offset,
+        'where': '1=1',
+        'outFields': outfields,
+        'outSR': '4326',
+        'f': 'json',
+        'returnGeometry': False,
+    }
+
+def get_data(url, outfields='*'):
+    LIMIT = 1000
+    offset = 0
+    data = []
+    first = True
+
+    while first or ('exceededTransferLimit' in resp_json and resp_json['exceededTransferLimit']):
+        first = False
+
+        r = requests.get(url, params=get_params(offset, outfields))
+        r.raise_for_status()
+
+        resp_json = r.json()
+
+        for row in resp_json['features']:
+            data.append(row['attributes'])
+
+        offset += LIMIT
+        # print(offset)
+
+    return data
+
+def ingest_service(url, file_name, outfields='*'):
+    path = f'source/{file_name}.json'
+    data = get_data(url, outfields)
+    with open(path, 'wt', encoding='utf-8') as file:
+        file.write(json.dumps(data))
+    
+
+SERVICES = [
+    (
+        'https://services3.arcgis.com/dty2kHktVXHrqO8i/arcgis/rest/services/Civil_Tickets/FeatureServer/0/query',
+        'civil_tickets',
+    ),
+    (
+        'https://services3.arcgis.com/dty2kHktVXHrqO8i/arcgis/rest/services/Violation_Status_History/FeatureServer/0/query',
+        'violation_status_history',
+    ),
+    (
+        'https://services3.arcgis.com/dty2kHktVXHrqO8i/arcgis/rest/services/Complaint_Violation_Notices/FeatureServer/0/query',
+        'complaint_violations',
+    ),
+    (
+        'https://services3.arcgis.com/dty2kHktVXHrqO8i/arcgis/rest/services/Data_311/FeatureServer/0/query',
+        'complaints_311'
+    ),
+    (
+        'https://services3.arcgis.com/dty2kHktVXHrqO8i/arcgis/rest/services/CDPH_Complaints/FeatureServer/0/query',
+        'complaints_health'
+    ),
+    (
+        'https://services3.arcgis.com/dty2kHktVXHrqO8i/arcgis/rest/services/Parcel_Analytics_(PUBLIC_DRAFT_)/FeatureServer/0/query',
+        'parcels',
+        [
+            'parcelpin', 
+            'par_addr_all',
+            'parcel_owner', 
+            'std_deeded_owner', 
+            'grantor', 
+            'grantee',
+            'last_transfer_date',
+            'tax_assessed_total',
+            'activerentalregistrationflag',
+            'activecertificateapprovingrentaloccupancyflag',
+            'leadsafecertificateactiveflag',
+            'transfers_in_5y',
+            'myplacelink',
+            'survey2022_grade',
+            'survey2022_photolink',
+            'taxdelinquencyamount',
+            'max_age',
+            'min_age',
+            'min_com_age',
+            'max_com_age',
+        ],
+    )
+]
+
+for service in SERVICES:
+    print('Ingesting:', service[1])
+    ingest_service(*service)
---
+++ pipeline/transform.py
@@ -0,0 +1,175 @@
+import pandas as pd
+import re 
+
+
+# PARCELS ----------------------------------------------------------------------
+
+parcels = pd.read_json('source/parcels.json')
+parcels.columns = [x.lower().strip() for x in parcels.columns]
+
+parcels = parcels[~parcels.par_addr_all.isna()].rename(columns={'parcelpin': 'parcel'})
+parcels['owner_clean'] = parcels['std_deeded_owner'].apply(
+    lambda x: re.sub(
+        r"\s\s+",
+        " ",
+        re.sub(
+            r"[^A-Za-z0-9]", 
+            " ", 
+            x
+        )
+    )
+    if not pd.isna(x) else None
+)
+parcels = parcels.copy()
+
+
+# CIVIL TICKETS ----------------------------------------------------------------------
+
+civil_tickets = pd.read_json('source/civil_tickets.json')
+civil_tickets.columns = [x.lower().strip() for x in civil_tickets.columns]
+
+civil_tickets = civil_tickets.rename(columns={'dw_parcel': 'parcel'})
+civil_tickets = civil_tickets[~civil_tickets.parcel.isna()]
+
+civil_tickets[[
+    'ticket_id',
+    'file_date',
+    'ticket_status',
+    'ticket_status_date',
+    'issue_date',
+    'additional_citation_details',
+    'parcel',
+]].to_csv('transformed/civil_tickets.csv', index=False)
+
+civil_tickets_agg = (civil_tickets
+    .groupby('parcel')
+    .size()
+    .reset_index()
+    .rename(columns={0: 'civil_tickets'})
+)
+
+parcels = parcels.merge(civil_tickets_agg, on='parcel', how='left')
+
+
+# COMPLAINTS (HEALTH) ----------------------------------------------------------------------
+
+complaints_health = pd.read_json('source/complaints_health.json')
+complaints_health.columns = [x.lower().strip() for x in complaints_health.columns]
+
+complaints_health = complaints_health.rename(columns={'dw_parcel': 'parcel'})
+complaints_health[[
+    'id',
+    'complaint_number',
+    'submit_date',
+    'complaint_type',
+    'complaint_input',
+    'complaint_inspector',
+    'complaint_status',
+    'complaint_outcome',
+    'food_complaint',
+    'farm_animal',
+    'insect_vermin',
+    'odor_type',
+    'odor_strength',
+    'problem_location_name',
+    'parcel',
+]].to_csv('transformed/complaints_health.csv', index=False)
+
+complaints_health_agg = (complaints_health
+    .groupby('parcel')
+    .size()
+    .reset_index()
+    .rename(columns={0: 'complaints_health'})
+)
+parcels = parcels.merge(complaints_health_agg, on='parcel', how='left')
+
+
+# VIOLATIONS ----------------------------------------------------------------------
+
+complaint_violations = pd.read_json('source/complaint_violations.json')
+complaint_violations.columns = [x.lower().strip() for x in complaint_violations.columns]
+
+complaint_violations = (complaint_violations
+    [['violation_number', 'record_id']]
+    .rename(columns={
+        'record_id': 'complaint_id',
+        'violation_number': 'record_id',
+    })
+    .drop_duplicates('record_id')
+)
+
+violation_status_history = pd.read_json('source/violation_status_history.json')
+violation_status_history.columns = [x.lower().strip() for x in violation_status_history.columns]
+
+violation_status_history = violation_status_history[~violation_status_history.task_status.isna()]
+violation_status_history['task_status'] = violation_status_history['task_status'].apply(lambda x: x.strip())
+violation_status_history = violation_status_history[violation_status_history.task_status == 'VN Created/Mailed']
+
+violation_status_history = (violation_status_history
+    [[
+        'record_id', 
+        'dw_parcel',
+        'type_of_violation',
+        'occupancy_or_use',
+        'issue_date',
+        'accela_citizen_access_url',
+    ]]
+    .sort_values(['record_id', 'dw_parcel'])
+    .drop_duplicates()
+    .rename(columns={'dw_parcel': 'parcel'})
+    .merge(complaint_violations, on='record_id', how='left')
+)
+violation_status_history.to_csv('transformed/code_violations.csv', index=False)
+
+violations_agg = (violation_status_history
+    .groupby('parcel')
+    .size()
+    .reset_index()
+    .rename(columns={0: 'code_violations'})
+)
+
+parcels = parcels.merge(violations_agg, on='parcel', how='left')
+
+
+# COMPLAINTS (311) ----------------------------------------------------------------------
+
+complaints_311 = pd.read_json('source/complaints_311.json')
+complaints_311.columns = ['_'.join(x.lower().strip().split()) for x in complaints_311.columns]
+
+complaints_311 = complaints_311.rename(columns={'parcelpin': 'parcel'})
+complaints_311 = complaints_311[~complaints_311.parcel.isna()]
+complaints_311 = complaints_311[complaints_311.agency_responsible == 'Building & Housing']
+complaints_311 = complaints_311[[
+    'service_request_id',
+    'service_name',
+    'status_description',
+    'requested_datetime',
+    'closed_date',
+    'source',
+    'target_date',
+    'parcel',
+]]
+
+complaints_311.to_csv('transformed/complaints_311.csv', index=False)
+
+complaints_311_agg = (complaints_311
+    .groupby('parcel')
+    .size()
+    .reset_index()
+    .rename(columns={0: 'complaints_311'})
+)
+parcels = parcels.merge(complaints_311_agg, on='parcel', how='left')
+
+
+# WRITE PARCELS ----------------------------------------------------------------------
+
+metrics = [
+    'code_violations', 
+    'complaints_health', 
+    'civil_tickets',
+    'complaints_311',
+]
+
+parcels[metrics] = parcels[metrics].fillna(value=0)
+
+parcels.to_csv('transformed/parcels.csv', index=False)
