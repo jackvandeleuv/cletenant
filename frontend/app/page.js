d99cfb4a3a7083441a74cabf634aa2256d3d15c8
jackvandeleuv <106289788+jackvandeleuv@users.noreply.github.com>
2026-05-30 09:01:03 -0400

    update name

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -43,7 +43,7 @@ export default function MainSearch() {
                 <svg className={styles.homeIcon} xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960">
                     <path d="M240-200h120v-240h240v240h120v-360L480-740 240-560v360Zm-80 80v-480l320-240 320 240v480H520v-240h-80v240H160Zm320-350Z"/>
                 </svg>
-                <h1 className={styles.siteHeaderName}>CleScout</h1>
+                <h1 className={styles.siteHeaderName}>CleTenant</h1>
             </div>
             <div className={styles.searchBoxWrapper}>
                 <input
