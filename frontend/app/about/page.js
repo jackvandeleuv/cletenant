5f0ac64c121dc01b91ad519c4729f06aa5062c68
jackvandeleuv <106289788+jackvandeleuv@users.noreply.github.com>
2026-06-07 19:41:12 -0400

    shorten

--- frontend/app/about/page.js
+++ frontend/app/about/page.js
@@ -56,7 +56,7 @@ export default function About() {
         },
         {
             title: 'Violation Status History',
-            body: '"A history of statuses for every building code violation record in City Accela records. This dataset provides one with a snapshot of the lifecycle of a given violation over time."',
+            body: '"A history of statuses for every building code violation record in City Accela records."',
             icon: (
                 <svg className={styles.icon} xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960">
                     <path d="m880-194-80-80v-326H474l-74-74v-86h-86l-80-80h246v160h400v486ZM820-28l-94-92H80v-648l-52-52 56-56L876-84l-56 56ZM160-200h80v-80h-80v80Zm0-160h80v-80h-80v80Zm0-160h80v-80h-80v80Zm160 320h80v-80h-80v80Zm0-160h80v-80h-80v80Zm160 160h166l-80-80h-86v80Zm240-240h-80v-80h80v80Z"/>
